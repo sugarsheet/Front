effc021044d70730247b4352825ec627f1755ef4
Jiwon EUN <jiwon.eun@gmail.com>
2021-05-14 16:28:06 +0200

    enfin?

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "lists#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :lists, only: [ :index, :show, :new, :create ] do
+  resources :lists, only: [ :index, :show, :new, :create, :destroy ] do
     resources :bookmarks, only: [ :new, :create ]
   end
   resources :bookmarks, only: [ :destroy ]
